f5794ef5cdabdab6b39f3bc5109996a0ba050a3a
pung8146 <pung8146@naver.com>
2022-10-15 13:27:48 +0900

    Array.Form 으로 배열을 만들기 , foreach로 이벤트 각자 할당해주기

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const canvas = document.querySelector("canvas");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 const lineWidthValue = document.getElementById("line-width");
 const colorValue = document.getElementById("color");
 const ctx = canvas.getContext("2d");
@@ -34,6 +37,12 @@ function onColorChange(event) {
   ctx.strokeStyle = event.target.value;
 }
 
+function changeColorOption(event) {
+  const colorPic = event.target.dataset.color;
+  ctx.strokeStyle = colorPic;
+  colorValue.value = colorPic;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
@@ -41,3 +50,7 @@ canvas.addEventListener("mouseleave", onMouseUp);
 
 lineWidthValue.addEventListener("change", onLineWidthChange);
 colorValue.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) =>
+  color.addEventListener("click", changeColorOption)
+);
