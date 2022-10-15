47915652957fb8702337bad6ec99dd199ec54aad
pung8146 <pung8146@naver.com>
2022-10-15 11:01:03 +0900

    mousemove 에 따라 컬러선이 그려집니다.

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 몸
-ctx.fillRect(200, 100, 15, 150);
-ctx.fillRect(250, 100, 60, 150);
-ctx.fillRect(340, 100, 15, 150);
-// 헤드
-ctx.arc(280, 60, 40, 0, 2 * Math.PI);
-ctx.fill();
+ctx.lineWidth = 2;
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260, 50, 10, Math.PI, 2 * Math.PI);
-ctx.arc(300, 50, 10, Math.PI, 2 * Math.PI);
-ctx.arc(280, 50, 5, 0, 2 * Math.PI);
-ctx.fill();
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#ffc3c3",
+  "#c56cf0",
+  "#fff200",
+  "#32ff72",
+  "#7d5fff",
+];
+
+function onClick(event) {
+  ctx.beginPath();
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.moveTo(0, 0);
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
