8f8bfaa8bc9763db468a8f358cb6659f0186ec54
pung8146 <pung8146@naver.com>
2022-10-15 12:07:20 +0900

    선의 굵기를 조절 하는 기능입니다.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const canvas = document.querySelector("canvas");
-
+const lineWidthValue = document.getElementById("line-width");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidthValue.value;
 let isPainting = false;
 function onMove(event) {
   if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
   }
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -24,7 +25,13 @@ function onMouseUp() {
   isPainting = false;
 }
 
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", onMouseUp);
+
+lineWidthValue.addEventListener("change", onLineWidthChange);
